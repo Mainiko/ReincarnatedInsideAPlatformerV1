b9bb6d335c8c7e6093325005f4dcbf43036ce87d
Lexan626 <alexiz.karlsson@hotmail.com>
2023-06-13 20:36:10 +0200

    Added schrodingers box

--- IsekaiPlatformerGD4/Scenes/Extra/box_that_disappears.cs
+++ IsekaiPlatformerGD4/Scenes/Extra/box_that_disappears.cs
@@ -4,16 +4,17 @@ using System.Threading.Tasks;
 
 public partial class box_that_disappears : StaticBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    // Called when the node enters the scene tree for the first time.
+    public  override void _Ready()
 	{
-	}
+    }
 
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public  override void _Process(double delta)
+	{
+       
+    }
 
     private async void _on_hitbox_body_entered(CharacterBody2D body)
     {
--- IsekaiPlatformerGD4/Scenes/Levels/BaseLevel.cs
+++ IsekaiPlatformerGD4/Scenes/Levels/BaseLevel.cs
@@ -3,65 +3,65 @@ using System;
 
 public partial class BaseLevel : Node
 {
-    // Called when the node enters the scene tree for the first time.
-    PackedScene playerScene;
+	// Called when the node enters the scene tree for the first time.
+	PackedScene playerScene;
 
-    Node2D PlayerRoot = null;
-    Node2D BallEnemyRoot = null;
+	Node2D PlayerRoot = null;
+	Node2D BallEnemyRoot = null;
 
-    public override void _Ready()
+	public override void _Ready()
 	{
-        //playerScene = GD.Load<PackedScene>("res://Scenes/Characters/Player.tscn");
-        //Node instance = playerScene.Instantiate();
-        //PlayerRoot = GetNode<Node2D>("PlayerRoot");
-        //PlayerRoot.AddChild(instance);
+		//playerScene = GD.Load<PackedScene>("res://Scenes/Characters/Player.tscn");
+		//Node instance = playerScene.Instantiate();
+		//PlayerRoot = GetNode<Node2D>("PlayerRoot");
+		//PlayerRoot.AddChild(instance);
 
 
-        InstanceScene("res://Scenes/Characters/Player.tscn", "PlayerRoot");
-        InstanceScene( "res://Scenes/Enemies/ball_enemy.tscn", "BallEnemyRoot");
+		InstanceScene("res://Scenes/Characters/Player.tscn", "PlayerRoot");
+		InstanceScene( "res://Scenes/Enemies/ball_enemy.tscn", "BallEnemyRoot");
 
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
 
 	}
 
-    //Instansiates and adds new scene as child into our scene
-    public void InstanceScene( string path, string rootname) 
-    {
+	//Instansiates and adds new scene as child into our scene
+	public void InstanceScene( string path, string rootname) 
+	{
 		//GD.Print(sceneRoot.Name.ToString());
 
-        PackedScene packedScene = GD.Load<PackedScene>(path);
+		PackedScene packedScene = GD.Load<PackedScene>(path);
 
-        Node2D sceneRoot;
+		Node2D sceneRoot;
 
 		Node instance = packedScene.Instantiate();
 		sceneRoot = GetNode<Node2D>(rootname);
 		sceneRoot.AddChild(instance);
 
-    }
+	}
 
-    public void SpawnPlayer()
-    {
-        InstanceScene( "res://Scenes/Characters/player.tscn", "PlayerRoot");
-    }
+	public void SpawnPlayer()
+	{
+		InstanceScene( "res://Scenes/Characters/player.tscn", "PlayerRoot");
+	}
 
 	public void OnPlayerDiedEvent()
 	{
-        InstanceScene("res://Scenes/Characters/player.tscn", "PlayerRoot");
+		InstanceScene("res://Scenes/Characters/player.tscn", "PlayerRoot");
 
-    }
+	}
 
 
-    public void ConnectFunc()
-    {
-        Callable callable = new Callable(this, nameof(OnPlayerDiedEvent));
-        this.Connect("OnPlayerDiedEventHandler", callable);
-        //this.Connect("OnPlayerDiedEventHandler", this, "OnPlayerDied");
-    }
+	public void ConnectFunc()
+	{
+		Callable callable = new Callable(this, nameof(OnPlayerDiedEvent));
+		this.Connect("OnPlayerDiedEventHandler", callable);
+		//this.Connect("OnPlayerDiedEventHandler", this, "OnPlayerDied");
+	}
 
 
 

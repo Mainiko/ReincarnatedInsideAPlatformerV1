acc77846c89fbe9274236e1b3dc7ff8305575841
Lexan626 <alexiz.karlsson@hotmail.com>
2023-07-05 00:02:38 +0200

    Added IU menu and a Boss battle!

---
+++ IsekaiPlatformerGD4/Scenes/Enemies/bossTest1.cs
@@ -0,0 +1,164 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class bossTest1 : CharacterBody2D
+{
+    [Export] private int Speed = 100;
+    [Export] private int timebeforeCharging = 1000;
+    [Export] private int timebeforeMoving = 100;
+    [Export] private int timeNeddedtoHit = 5;
+
+
+
+    int timesPlayerHaveHit = 0;
+    int timeSinceStoped = 0;
+    bool spikeIsActive = false;
+
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    Vector2 direction = Vector2.Left;
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        Area2D Spikes = GetNode<Area2D>("HitboxDeath");
+
+
+        if (IsOnWall())
+        {
+            if (timesPlayerHaveHit > 0)
+            {
+                Speed = 0;
+
+                if (timeSinceStoped > timebeforeMoving)
+                {
+                    Speed = 100;
+                    spikeIsActive = false;
+                    timeSinceStoped = 0;
+                }
+
+                timeSinceStoped += 1;
+            }
+            
+
+            direction *= -1;
+
+        }
+        if (spikeIsActive)
+        {
+            Spikes.Visible = true;
+            Spikes.CollisionLayer = 1;
+        }
+        else if (!spikeIsActive)
+        {
+            Spikes.Visible = false;
+            Spikes.CollisionLayer = 0;
+        }
+        velocity.x = direction.x * Speed;
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+    private async void _on_hitbox_body_entered(CharacterBody2D body)
+    {
+        GD.Print("Body: " + body.Name + "has entered");
+        GD.Print("_on_hitbox_body_entered");
+
+
+        Area2D Spikes = GetNode<Area2D>("HitboxDeath");
+       // CharacterBody2D player = GetParent().GetNode<CharacterBody2D>("player");
+        var player = GetNode<CharacterBody2D>(body.GetPath());
+
+        //CharacterBody2D Boss = GetNode<CharacterBody2D>("boss");
+
+
+
+        //Call function from player
+        if (body.Name == "player" && !spikeIsActive)
+        {
+            GD.Print("Kill me please!");
+            player.Call("PlayerJumpOnEnemy");
+            spikeIsActive = true;
+            Spikes.Visible = true;
+            Spikes.CollisionLayer = 1;
+            Speed = 0;
+            timesPlayerHaveHit++;
+
+            if (timesPlayerHaveHit == timeNeddedtoHit)
+            {
+                this.QueueFree();
+            }
+
+            await Task.Delay(timebeforeCharging);
+            GD.Print(player.Position);
+            GD.Print(this.Position);
+            GD.Print(direction);
+
+            if (player.Position.x > this.Position.x)
+            {
+                GD.Print("right");
+                if(direction.x > 0)
+                {
+                    //do nothing
+                }
+
+                else if(direction.x < 0)
+                {
+                    direction.x *= -1;
+                }
+            }
+
+            else if(player.Position.x < this.Position.x)
+            {
+                GD.Print("left");
+
+                if (direction.x > 0)
+                {
+                    direction.x *= -1;
+                }
+
+                else if (direction.x < 0)
+                {
+                    //do nothing
+                }
+            }
+
+            Speed = 200;
+
+        }
+    }
+
+        private void _on_hitbox_death_body_entered(CharacterBody2D body)
+    {
+        GD.Print("Body: " + body.Name + "has entered");
+        GD.Print("_on_hitbox_death_body_entered");
+
+
+
+        //Call function from player
+        if (body.Name == "player" && spikeIsActive)
+        {
+
+            var player = GetNode<CharacterBody2D>(body.GetPath());
+            player.Call("PlayerDie");
+        }
+    }
+    
+    private void _on_hitboxfrontandback_body_entered(CharacterBody2D body)
+    {
+        GD.Print("Body: " + body.Name + "has entered");
+
+        GD.Print("_on_hitboxfrontandback_body_entered");
+
+        //Call function from player
+        if (body.Name == "player")
+        {
+            var player = GetNode<CharacterBody2D>(body.GetPath());
+            player.Call("PlayerDie");
+        }
+    }
+
+}
---
+++ IsekaiPlatformerGD4/Scenes/UI/menu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class menu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_start_button_pressed()
+    {
+        GetTree().ChangeSceneToFile("res://Scenes/Levels/StartWorldtest.tscn");
+		GD.Print("Game start");
+	}
+
+	public void _on_quit_button_pressed()
+	{
+		GetTree().Quit();
+		GD.Print("Quit game");
+	}
+
+}
